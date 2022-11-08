98b83739caf50a2c80e2440422fde9b044f90e05
Matthew Smith <mattysmith22@googlemail.com>
2022-11-08 23:02:41 +0000

    Added support for constant value types

--- src/TQF/Type.hs
+++ src/TQF/Type.hs
@@ -1,19 +1,24 @@
-{-# LANGUAGE TypeFamilies, StandaloneDeriving, FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies, FlexibleInstances #-}
 module TQF.Type
     ( Within(..)
-    , Type(..)
-    , BaseType(..)
+    , Type
+    , BaseType
     , SimpleType(..)
+    , ConstType
     
     , bottom
     , top
     , simpleType
-    )where
+    , constNumber
+    , constString
+    , constBool
+    ) where
 
-import Data.Functor.Identity
+import Data.Functor.Identity ()
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.List.Extra
+import Test.QuickCheck
 
 class Within a where
     isWithin :: a -> a -> Bool
@@ -21,20 +26,53 @@ class Within a where
 data Type
     = Top
     | Options (Set BaseType)
-    deriving (Ord, Eq, Show)
+    deriving (Show)
 
+instance Eq Type where
+    l == r = l `isWithin` r && r `isWithin` l
 instance Within Type where
     _ `isWithin` Top = True
     Top `isWithin` _ = False
     (Options s) `isWithin` (Options l) = all (\x -> any (x `isWithin`) l) s
 
-newtype BaseType
+instance Arbitrary Type where
+    arbitrary = frequency
+        [ (1, return Top)
+        , (19, arbitrary)
+        ]
+
+data BaseType
     = SimpleType SimpleType
+    | ConstType ConstType
     deriving (Ord, Eq, Show)
+
+instance Arbitrary BaseType where
+    arbitrary = oneof
+        [ SimpleType <$> elements [minBound..maxBound]
+        , ConstType <$> arbitrary
+        ]
 
 instance Within BaseType where
     SimpleType s `isWithin` SimpleType l = s == l
+    ConstType s `isWithin` ConstType l = s == l
+    ConstType s `isWithin` SimpleType l = typeOfConst s == l
+    _ `isWithin` _ = False
 
+
+typeOfConst :: ConstType -> SimpleType
+typeOfConst (ConstNumber _) = Number
+typeOfConst (ConstString _) = String
+typeOfConst (ConstBool _) = Bool
+
+data ConstType = ConstNumber Double | ConstString String | ConstBool Bool
+    deriving (Ord, Eq, Show)
+
+instance Arbitrary ConstType where
+    arbitrary = oneof
+        [ ConstNumber <$> arbitrary
+        , ConstString <$> arbitrary
+        , ConstBool <$> arbitrary
+        ]
 data SimpleType = Number | String | Bool
     deriving (Ord, Eq, Show, Bounded, Enum)
 
@@ -54,3 +92,12 @@ top = Top
 
 simpleType :: SimpleType -> Type
 simpleType = Options . Set.singleton . SimpleType
+
+constNumber :: Double -> Type
+constNumber = Options . Set.singleton . ConstType . ConstNumber
+
+constString :: String -> Type
+constString = Options . Set.singleton . ConstType . ConstString
+
+constBool :: Bool -> Type
+constBool = Options . Set.singleton . ConstType . ConstBool
--- test/TQF/TypeSpec.hs
+++ test/TQF/TypeSpec.hs
@@ -5,18 +5,7 @@ module TQF.TypeSpec
 import Test.Hspec
 import Test.QuickCheck
 import TQF.Type
-import Test.QuickCheck (Arbitrary(arbitrary))
 
-instance Arbitrary Type where
-    arbitrary = frequency
-        [ (1, return Top)
-        , (9, arbitrary)
-        ]
-instance Arbitrary BaseType where
-    arbitrary = oneof
-        [ SimpleType <$> elements [minBound..maxBound]
-        ]
-    
 spec :: Spec
 spec = do
     describe "monoid" $ do
@@ -30,11 +19,26 @@ spec = do
         it "Should be commutative" $ do
             simpleType String <> simpleType Number `shouldBe` simpleType Number <> simpleType String
     describe "isWithin" $ do
-        it "Should class anything being within Top" $ do
-            shouldSatisfy (top `isWithin` top) id
-            shouldSatisfy (bottom `isWithin` top) id
-            shouldSatisfy (simpleType String `isWithin` top) id
-            shouldSatisfy ((simpleType String <> simpleType Number) `isWithin` top) id
-        it "(property) items should always be within their merged counterpart" $ do
-            let prop = ((\(l, r) -> l `isWithin` (l <> r) && r `isWithin` (l <> r)) :: (Type, Type) -> Bool)
-            forAll arbitrary prop
+        it "Should class anything being within Top" $
+               (top `isWithin` top)
+            && (bottom `isWithin` top)
+            && (simpleType String `isWithin` top)
+            && ((simpleType String <> simpleType Number) `isWithin` top)
+        it "(property) items should always be within their merged counterpart" $
+            forAll (arbitrary :: Gen (Type, Type)) (\(l, r) -> l `isWithin` (l <> r) && r `isWithin` (l <> r))
+    describe "Simple type" $ do
+        it "Should class two types that are equal to be within it" $
+            simpleType String `isWithin` simpleType String
+        it "Should class two simple types that are not equal to not be within each other" $
+            not $ simpleType String `isWithin` simpleType Number
+    describe "Constant types" $ do
+        it "Should class two equal constant types to be within each other" $
+            constNumber 1 `isWithin` constNumber 1
+            && constString "a" `isWithin` constString "a"
+            && constBool True `isWithin` constBool True
+        it "Should class two unequal constant type values of the same type to be equal" $
+            not (constNumber 1 `isWithin` constNumber 2)
+            && not (constString "a" `isWithin` constString "b")
+            && not (constBool True `isWithin` constBool False)
+        it "Should class two constant types values of different types to nto be within" $
+            not (constNumber 1 `isWithin` constString "a")
