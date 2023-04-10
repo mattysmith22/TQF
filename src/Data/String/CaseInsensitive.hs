59d7ee503bd605da45954697c4b61d6c74c6aa17
Matthew Smith <mattysmith22@googlemail.com>
2023-04-10 23:33:32 +0100

    Removed unused module

--- src/Data/String/CaseInsensitive.hs
+++
@@ -1,8 +0,0 @@
-module Data.String.CaseInsensitive where
-import           Data.Char
-
-caseInsensEq :: String -> String -> Bool
-caseInsensEq x y = map toLower x == map toLower y
-
-(=~=) :: String -> String -> Bool
-(=~=) = caseInsensEq
